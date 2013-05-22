0ed0d916fd3637d1c280c0846debc6a4dfbdf6a3
yangjw <yangjw.kai7@gmail.com>
2013-05-22 14:19:29 +0800

    Initial Commit

---
+++ DrawRect/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DrawRect
+//
+//  Created by yangjw  on 13-5-22.
+//  Copyright (c) 2013年 yangjw . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
